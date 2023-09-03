bfd2986ca10ddff21b8af5de09d20501feb75fd9
John-RX <143143788+John-RX@users.noreply.github.com>
2023-09-03 00:29:36 +0000

    HW1: Multiply Numbers Func and Test Cases

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,23 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "data_types.h"
 
 //write namespace using statement for cout
-
+using std::cin; using std::cout;
 
 int main()
 {
-	
+	int num;
+	cout << "Enter a number for variable 'num': ";
+	cin >> num;
+	int result;
+	result = multiply_numbers(num);
+	cout << "Multiplying that variable equals: ";
+	cout << result << "\n";
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout << "Multiplying a new variable equals: ";
+	cout << result << "\n";
 
 	return 0;
 }
