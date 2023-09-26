245d55329cfde6bebacbb61c4c2bd12db98925dc
John-RX <143143788+John-RX@users.noreply.github.com>
2023-09-26 00:59:29 +0000

    This is my commit message as requested for HW3

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,8 +1,69 @@
 //write include required statements
-
+#include<iostream>
+#include<iomanip>
+using namespace std;
 
 //Write a function(s) prototype  here
+int get_letter_grade_using_if()
+{
+    int numberGrade;
+    cout << "What is your current grade in this course?: ";
+    cin >> numberGrade;
 
+    if ((numberGrade <=100) && (numberGrade >= 90))
+        cout << "You have an A!  Congratulations!";
+    
+    else if (numberGrade >= 80)
+        cout << "You have a B.  Keep working at it!";
 
+    else if (numberGrade >= 70)
+        cout << "You have a C.  You can do better.";
 
+    else if (numberGrade >= 60)
+        cout << "You have a D.  You should ask for help.";
+    
+    else if (numberGrade >= 0)
+        cout << "You have an F.  You are failing!";
+    
+    else
+        cout << "You have entered an invalid grade number!";
+    
+    return 0;
+    }
 
+int get_letter_grade_using_switch()
+{
+    int numberGrade, calculatedGrade;
+    cout << "Hello, this terminal will translate your academic\n";
+    cout << "number grade into a letter grade\n\n";
+    cout << "Please enter your grade: ";
+    cin >> numberGrade;
+
+    calculatedGrade = (numberGrade / 10);
+
+    switch(calculatedGrade)
+    {
+        case 10: case 9:
+            cout << "You have an A!  Congratulations!";
+            break;
+
+        case 8:
+            cout << "You have a B.  Keep working at it!";
+            break;
+
+        case 7:
+            cout << "You have a C.  You can do better.";
+            break;
+        
+        case 6:
+            cout << "You have a D.  You should ask for help.";
+            break;
+        
+        case 5: case 4: case 3: case 2: case 1: case 0:
+            cout << "You have an F.  You are failing!";
+            break;
+        
+        default: cout << "That is an invalid grade.  Try again.";
+    }
+    return 0;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,23 @@
 //write include statements
+#include<iostream>
+#include<iomanip>
+#include<decisions.h>
+using namespace std;
 
 int main() 
 {
+	int choice;
+
+	cout << "Welcome to the Grade Translator App\n";
+	cout << "The easiest way to translate your grade!\n\n";
+	cout << "Press '1' to translate your grade by 'if-else':\n";
+	cout << "Press '2' to translate your grade by menu:\n";
+	cin >> choice;
+
+	if (choice == 1)
+		get_letter_grade_using_if();
+	if (choice == 2)
+		get_letter_grade_using_switch();
+		
 	return 0;
 }
