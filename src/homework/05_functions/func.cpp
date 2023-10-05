7c1c4041b65cd54e0e6a30e4065da0cd7891fa62
John-RX <143143788+John-RX@users.noreply.github.com>
2023-10-05 04:33:11 +0000

    Sorry I had trouble in this chapter.

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,55 @@
 //add include statements
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
 
 //add function code here
+double get_gc_content(string dna_sequence)
+{
+    int dna_length = dna_sequence.length(); //length of sequence is 8
+    int gc_count; //needed to tally how many g&c nucleotides there are
+    for (int cycle = 1; cycle <= dna_length; cycle++) //(int cycle =1; cycle <= 8; +1 every loop)
+    {
+        char nucleotide = dna_sequence[(cycle-1)]; //get the nucleotide at position (cycle - 1)
+        char upper_nucleotide = toupper(nucleotide); //makes nucleotides uppper case
+        if (upper_nucleotide == 'G' || upper_nucleotide == 'C')
+            gc_count += 1; //find how many GCs are in the sequence
+    }
+    double gc_content = (gc_count/dna_length); //do the division for GC decimal
+    return gc_content; //return GC decimal to the caller
+
+}
+
+string get_dna_complement(string dna_sequence) //first find a way to read each char
+{
+    int dna_length = dna_sequence.length(); //needed for the loop
+    string dna_complement; //need this prepared to add complement characters to
+    for (int cycle = 1; cycle <= dna_length; cycle++)
+        {
+            char nucleotide = dna_sequence[(cycle-1)]; //get nucleotide at this position
+            char upper_nucleotide = toupper(nucleotide); //make it uppercase just in case
+            if (upper_nucleotide == 'G')
+                dna_complement += 'C'; //turn Gs into Cs, Cs into Gs, As into Ts, and Ts into As
+            else if (upper_nucleotide == 'C')
+                dna_complement += 'G';
+            else if (upper_nucleotide == 'A')
+                dna_complement += 'T';
+            else if (upper_nucleotide == 'T')
+                dna_complement += 'A';
+        }
+    return dna_complement;
+}
+
+string get_reverse_string(string dna_sequence)
+{
+    int dna_length = dna_sequence.length();
+    string dna_reversed;
+    for (int cycle = dna_length; cycle >= 1; cycle--)
+    {
+        char nucleotide = dna_sequence[(cycle-1)];
+        char upper_nucleotide = toupper(nucleotide);
+        dna_reversed += upper_nucleotide;
+    }
+    return dna_reversed;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,48 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<func.h>
+using namespace std;
 
 int main() 
 {
-
+	int choice;
+	string dna_sequence;
+	string dna_calc_gc_content;
+	string dna_reversed_step1;
+	string dna_complement_step2;
+	do
+	{
+		cout << "Welcome to Gene-Analysis, for all your nucleotide needs:" << endl << endl;
+		cout << "Please pay attention as our menu options have changed" << endl;
+		cout << "To get the GC content of a DNA sequence, enter 1" << endl;
+		cout << "To Get the DNA complement of your sequence, enter 2" << endl;
+		cout << "To quit the program, enter 3";
+		cin >> choice;
+		if (choice == 1 || choice == 2 || choice == 3)
+		{
+			switch(choice)
+			{
+				case 1: 
+					cout << "Please enter your DNA sequence with no spaces or characters besides A, T, G, and C" << endl;
+					cin >> dna_sequence;
+					dna_calc_gc_content = get_gc_content(dna_sequence);
+					cout << "Your sequence's GC content is: " << dna_calc_gc_content << endl;
+					break;
+				case 2: 
+					cout << "Please enter your DNA sequence with no spaces or characters besides A, T, G, and C" << endl;
+					cin >> dna_sequence;
+					dna_reversed_step1 = get_reverse_string(dna_sequence);
+					dna_complement_step2 = get_dna_complement(dna_reversed_step1);
+					cout << "Your sequence's reverse complement is: " << dna_complement_step2 << endl;
+					break;
+			}
+		}
+		else
+		{
+			cout << "That is not a valid entry.  Please try again." << endl;
+		}
+	} 
+	while (choice != 3);
 	return 0;
 }
